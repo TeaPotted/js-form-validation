3593621fd9aba2544a48315927634c8733f7c42c
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-05-14 08:16:25 +0800

    get all the inputs and form element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css"
+
+// get all the inputs and form element
+const email = document.getElementById("mail");
+const country = document.getElementById("country");
+const postalCode = document.getElementById("postal-code");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirmation");
+const form = document.querySelector("form");
