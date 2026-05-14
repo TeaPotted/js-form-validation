1f6fdac68f7fcf19eef8f309d19e3c5760adfb9f
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-05-14 08:30:39 +0800

    create a function for checking validity of email input and call it when when email has an input and when form is submitted

--- src/index.js
+++ src/index.js
@@ -8,6 +8,34 @@ const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("password-confirmation");
 const form = document.querySelector("form");
 
+// function for checking validity of email input
+function checkEmail() {
+  switch (true) {
+    // if input is not a valid email
+    case email.validity.typeMismatch:
+      email.setCustomValidity("Invalid email address");
+      email.reportValidity(); // report the error
+      break;
+    
+    // if input is empty
+    case email.validity.valueMissing:
+      email.setCustomValidity("The email field needs to be filled!");
+      email.reportValidity();
+      break;
+
+    // else, reset email's customValidity message
+    default:
+      email.setCustomValidity("");
+      break;
+  };
+};
+
+// check email when there is an input
+email.addEventListener("input", (e) => checkEmail());
+
+// when form is submitted, check all the inputs
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // prevent the form from submitting
+  
+  checkEmail();
 });
